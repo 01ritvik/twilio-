8d47c1794a3cb832db38f533a183e985d16b4f40
01ritvik <60423384+01ritvik@users.noreply.github.com>
2021-09-29 16:02:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ auth_token = "200102726ea717d7e35454fcb74f9619"
 client = Client(accound_sid,auth_token)
 
 message = client.messages.create(
-    to="+918448344321",
+    to="your registered number",
     from_="+14153603343",
     body = "hii"
 
