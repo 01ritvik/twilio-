2c27291be10d7678ef7432c57f501441fdb2d516
01ritvik <60423384+01ritvik@users.noreply.github.com>
2021-10-25 12:39:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from twilio.rest import  Client
-accound_sid = "AC75c8a114a077e29ffa28b8c1796be8b3"
-auth_token = "200102726ea717d7e35454fcb74f9619"
+accound_sid = "AC75c8a114a077e29ffa28b8c1796be8b3" 
+auth_token = "200102726ea717d7e35454fcb74f9619"  #different for different number
 
 client = Client(accound_sid,auth_token)
 
 message = client.messages.create(
     to="your registered number",
-    from_="+14153603343",
+    from_="+14153603343", #twilio number
     body = "hii"
 
 print(message.sid)
